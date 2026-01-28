21d4ff00debb749fd3a8df3983130de2c9aba603
djonathan8016 <djonathan8016@gmail.com>
2026-01-28 11:27:34 -0300

    config: setuo eslit

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -7,9 +11,32 @@ export class UserService {
   constructor(private prisma: PrismaService) {}
 
   async create(dto: CreateUserDto) {
-    const userExist = await this.prisma.user.findUnique({
+    const userExists = await this.prisma.user.findUnique({
       where: { email: dto.email },
     });
-    return userExist;
+
+    if (userExists) {
+      throw new BadRequestException('User already exists');
+    }
+
+    return this.prisma.user.create({
+      data: dto,
+    });
+  }
+
+  async findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  async findById(id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not');
+    }
+
+    return user;
   }
 }
