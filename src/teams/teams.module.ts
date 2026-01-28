9c092fe4a34c491e8417c833b304529244578a0e
djonathan8016 <djonathan8016@gmail.com>
2026-01-28 18:16:55 -0300

    fix: remove unused teams modules

--- src/teams/teams.module.ts
+++ src/teams/teams.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TeamsController } from './teams.controller';
-import { Teams } from './teams';
-import { TeamsService } from './teams.service';
 import { TeamsService } from './teams.service';
 
 @Module({
   controllers: [TeamsController],
-  providers: [Teams, TeamsService]
+  providers: [TeamsService],
 })
 export class TeamsModule {}
